6d3b34978f93e0a36ec382f602599aafa1e7725e
ricardon91 <ricardo_l.h.p@hotmail.com>
2020-01-23 15:16:46 -0300

    Project Created

---
+++ src/main/java/com/ricardo/lojaapp/LojaappApplication.java
@@ -0,0 +1,13 @@
+package com.ricardo.lojaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaappApplication.class, args);
+	}
+
+}
