95ae6a359334f65ab7626098e8511adf1e84fb03
ricardon91 <ricardo_l.h.p@hotmail.com>
2020-01-23 15:53:48 -0300

    Category created

--- src/main/java/com/ricardo/lojaapp/resources/CategoryResource.java
+++ src/main/java/com/ricardo/lojaapp/resources/CategoryResource.java
@@ -1,16 +1,29 @@
 package com.ricardo.lojaapp.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ricardo.lojaapp.domain.Category;
 
 @RestController
 @RequestMapping(value="/categories")
 public class CategoryResource {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "REST está funcionando";
+	public List<Category> listar() {
+		
+		Category cat1 = new Category(1, "Computers");
+		Category cat2 = new Category(1, "Office");
+		
+		List<Category> list = new ArrayList<>();
+		list.add(cat1);
+		list.add(cat2);
+		
+		return list;
 	}
 
 }
